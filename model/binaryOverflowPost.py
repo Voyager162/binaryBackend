5c3571b520d2ba323c7b158aa8c89f5e6afb8776
JustinQ-DNHS <justin.q224@icloud.com>
2025-02-04 15:26:05 -0800

    integrated up and downvotes

--- model/binaryOverflowPost.py
+++ model/binaryOverflowPost.py
@@ -7,40 +7,34 @@ class BinaryOverflowPost(db.Model):
     __tablename__ = 'binaryPosts'
     
     id = db.Column(db.Integer, primary_key=True)
-    # Change this to db.ForeignKey('binaryPostContent._title')
     _title = db.Column(db.String(255), nullable=False)
-    ### CHANGE THIS TO _POST_ID, needs to be reworked in the case of deleted comments
-    ### CHANGE THIS TO _POST_ID, needs to be reworked in the case of deleted comments
-    ### CHANGE THIS TO _POST_ID, needs to be reworked in the case of deleted comments
     _post_ref = db.Column(db.Integer, db.ForeignKey('binaryPostContent.id'), nullable=False)
-    # Change this to db.ForeignKey("binaryPostContent._content")
     _blurb = db.Column(db.String(255), nullable=False)
     _author = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     _date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    _upvotes = db.Column(db.Integer, nullable=False)
+    _downvotes = db.Column(db.Integer, nullable=False)
+    _users_voted = db.Column(db.JSON, nullable=True)
     
     # HOW TO QUERY WITHOUT REFERENCING ID
     # # Get posts created after a specific date
     # specific_date = datetime(2025, 1, 1)
     # posts = Post.query.filter(Post.created_at > specific_date).all()
-
     # # Get posts updated after a specific date
     # recent_posts = Post.query.filter(Post.updated_at > datetime(2025, 2, 1)).all()
-    
-    # # WIP Features
-    # _upvotes = db.Column(db.Integer, db.ForeignKey('binaryPostCotnent._upvotes'), nullable=True)
-    # _downvotes = db.Column(db.Integer, db.ForeignKey('binaryPostContent._upvotes'), nullable=True)
-    # _users_upvoted = db.Column(db.JSON, db.ForeignKey('binaryPostContent._users_upvoted, nullable=True))
-    # _users_downvoted = db.Column(db.JSON, db.ForeignKey('binaryPostContent._users_upvoted), nullable=True)
-    
+        
     def __init__(self, post_ref):
         self._post_ref = post_ref
         post = BinaryOverflowContent.query.get(self._post_ref)
         self._title = post._title if post else None
         self._blurb = post._content if post else None
         self._author = post._author if post else None
+        self._upvotes = post._upvotes if post else None
+        self._downvotes = post._downvotes if post else None
+        self._users_voted = post._users_voted if post else None
 
     def __repr__(self):
-        return f'BinaryOverflowPosts(id={self.id}, title={self._title}, post_ref={self._post_ref}, blurb={self._blurb}, author={self._author}, date_posted={self._date_posted})'
+        return f'BinaryOverflowPosts(id={self.id}, title={self._title}, post_ref={self._post_ref}, blurb={self._blurb}, author={self._author}, date_posted={self._date_posted}, upvotes={self._upvotes}, downvotes={self._downvotes}, users_voted={self._users_voted})'
     
     def create(self):
         try:
@@ -58,7 +52,10 @@ class BinaryOverflowPost(db.Model):
             'post_ref': post.post_id if post else None,
             'blurb': post.content if post else None,
             'author': post.author if post else None,
-            'date_posted': self._date_posted
+            'date_posted': self._date_posted,
+            'upvotes': post.upvotes if post else None,
+            'downvotes': post.downvotes if post else None,
+            'users_voted': post.users_voted if post else None
         }
         
     def update(self, new_data):
@@ -68,8 +65,10 @@ class BinaryOverflowPost(db.Model):
         
         # Gets the new data and sets it equal to these variables
         title = new_data.get("title", "")
+        # Should never need to be updated, as it referenced post should always be same
         post_ref = new_data.get("post_ref", "")
         blurb = new_data.get("blurb", "")
+        # Update when user changes name, shouldn't be necessary with current implementation
         author = new_data.get("author", "")
         # This shouldn't ever need to be updated but will be added just in case
         date_posted = new_data.get("date_posted", "")
@@ -110,7 +109,6 @@ def initBinaryPosts():
         """Tester data for table"""
         
         p1 = BinaryOverflowPost(post_ref=1)  
-        #  p2 = BinaryOverflowPost(title='Binary Minithread', post_ref=1, blurb="placeholder", author=1, date_posted="2024/02/24")  
         
         for post in [p1]:
             try:
