46487a66a554a69b42e5263e3493c7c12c664cc5
JustinQ-DNHS <justin.q224@icloud.com>
2025-02-06 11:27:57 -0800

    added comments to code, removed states and parents due to new comments model, added a relationship to the binaryOverflowComments model

--- model/binaryOverflowContent.py
+++ model/binaryOverflowContent.py
@@ -1,37 +1,36 @@
 from sqlite3 import IntegrityError
 from sqlalchemy import Text
 from __init__ import app, db
+from datetime import timezone, datetime
 
 class BinaryOverflowContent(db.Model):
     __tablename__ = 'binaryPostContent'
-    
-    # IDEA FOR CREATING POST_ID. ADD ALL CURRENTLY EXISTING POST_ID TO A LIST AND .MAX() IT. (may need to convert to decimal first)
-    
+        
+    # Defined columns
     id = db.Column(db.Integer, primary_key=True)
     _title = db.Column(db.String(255), nullable=False)
-    _post_id = db.Column(db.String(255), nullable=False)
     _author = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    # Plan is for two types, either parent or child. If it's a child make sure to include the parent
-    _state = db.Column(db.String(255), nullable=False)
-    _parent = db.Column(db.Integer, nullable=True, default=None)
+    _date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
     _content = db.Column(db.String(255), nullable=False)
     _upvotes = db.Column(db.Integer, nullable=False, default=0)
     _downvotes = db.Column(db.Integer, nullable=False, default=0)
     _users_voted = db.Column(db.JSON, nullable=True, default=None)
     
-    def __init__(self, title, post_id, author, state, content, parent=None):
+    # Defined relationships
+    comments = db.relationship('BinaryOverflowComments', backref='comments', cascade='all, delete-orphan')
+    
+    # Initial parameters to create the class
+    def __init__(self, title, author, content):
         self._title = title
-        self._post_id = post_id
         self._author = author
-        self._state = state
-        self._parent = parent
         self._content = content
     
+    # Called when initally created (?). I think
     def __repr__(self):
-        return f'BinaryOverflowContent(id={self.id}, title={self._title}, post_id={self._post_id}, author={self._author} state={self._state}, parent={self._parent}, content={self._content}, upvotes={self._upvotes}, downvotes={self._downvotes}, users_voted={self._users_voted})'
+        return f'BinaryOverflowContent(id={self.id}, title={self._title}, author={self._author}, date_posted={self._date_posted}, content={self._content}, upvotes={self._upvotes}, downvotes={self._downvotes}, users_voted={self._users_voted})'
     
+    # Create a new post object
     def create(self):
-        # Create a new post object, this is to make building the page easier
         try:
             db.session.add(self)
             db.session.commit()
@@ -39,42 +38,31 @@ class BinaryOverflowContent(db.Model):
             db.session.rollback()
             raise e
         
+    # Reads the data and returns it in a dictionary, key-pair value format
     def read(self):
         return {
             'id': self.id,
             'title': self._title,
-            'post_id': self._post_id,
             'author': self._author,
-            'state': self._state,
-            'parent': self._parent,
+            'date_posted': self._date_posted,
             'content': self._content,
             'upvotes': self._upvotes,
             'downvotes': self._downvotes,
             'users_voted': self._users_voted            
         }
         
+    # Updates the data inside. Only updates things that should be updated using a PUT statement
     def update(self, new_data):
         # Checks if the new_data is in a dictionary format
         if not isinstance(new_data, dict):
             return self
         
-        # Gets the new data and sets it equal to these variables
+        # Gets the new data and sets it equal to these variables, these are the only things that should be changed using PUT
         title = new_data.get("title", "")
-        post_id = new_data.get("post_id", "")
-        ### These two values should never need to be updated, so please don't
-        state = new_data.get("state", "")
-        parent = new_data.get("parent", "")
-        ###
         content = new_data.get("content", "")
         
         if title:
             self._title = title
-        if post_id:
-            self._post_id = post_id
-        if state:
-            self._state = state
-        if parent:
-            self._parent = parent
         if content: 
             self._content = content
         
@@ -85,6 +73,7 @@ class BinaryOverflowContent(db.Model):
             return None
         return self
     
+    # Deletes the data
     def delete(self):
         try:
             db.session.delete(self)
@@ -92,6 +81,24 @@ class BinaryOverflowContent(db.Model):
         except Exception as e:
             db.session.rollback()
             raise e
+    
+    # Adds an upvote
+    def upvote(self):
+        # WIP NOT DONE
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
+    
+    # Adds a downvote
+    def downvote(self):
+        # WIP NOT DONE
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e 
         
 def initBinaryPostContent():
     with app.app_context():
@@ -99,7 +106,7 @@ def initBinaryPostContent():
         db.create_all()
         """Tester data for table"""
         
-        p1 = BinaryOverflowContent(title="Binary Minithread", post_id="000000", author=1,state="parent", content="This is the megathread for binary content")
+        p1 = BinaryOverflowContent(title="Binary Minithread", author=1, content="This is the megathread for binary content")
         
         for post in [p1]:
             try:
